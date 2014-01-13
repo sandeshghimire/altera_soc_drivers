bbc5f259d2e67bb33f50f9e71c67dd9b14e3a549
sandesh ghimire <sandeshghimire@outlook.com>
2014-01-13 13:21:47 -0800

    memory map

---
+++ src/include/mem_map.h
@@ -0,0 +1,9 @@
+#ifndef _MEM_MAP_H
+#define _MEM_MAP_H
+
+#define FPGASLAVES 		0xC0000000
+#define FPGASLAVES_SIZE 0x03C0
+#define HSP_PERIPH		0xFC000000
+#define LWFPGASLAVES	0x2
+
+#endif /*_MEM_MAP_H*/
